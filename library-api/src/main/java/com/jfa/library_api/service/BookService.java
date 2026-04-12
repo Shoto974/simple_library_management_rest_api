e73007a1185c6a5444e13e45edbd63aa0125a7f7
Jessy <jessy.fangui@gmail.com>
2026-04-12 15:10:11 +0200

    feat: implements Book service

---
+++ library-api/src/main/java/com/jfa/library_api/service/BookService.java
@@ -0,0 +1,51 @@
+package com.jfa.library_api.service;
+
+import com.jfa.library_api.repository.BookRepository;
+import entity.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final AtomicLong counter = new AtomicLong();
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(long id){
+        return bookRepository.findById(id).get();
+    }
+
+    public Book addBook(Book book){
+        Long id = counter.incrementAndGet();
+        Book newBook = new Book(id, book.title(), book.author(), book.year());
+        bookRepository.save(newBook);
+        return newBook;
+    }
+
+    public Book updateBook(Book book){
+        Book updatedBook = new Book(book.id(), book.title(), book.author(), book.year());
+        bookRepository.save(updatedBook);
+        return updatedBook;
+    }
+
+    public void deleteBook(long id){
+        bookRepository.deleteById(id);
+    }
+
+
+
+
+
+
+
+}
